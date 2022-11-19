2f64a83ae92468938f93960b52f8edb94b24d9ad
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2022-11-19 21:41:41 +0500

    Сделал все блоки и JS

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const popupElement = document.querySelector('.popup'); //Ищем попап в разметке
+const popupCloseButtonElement = popupElement.querySelector('.popup__close'); //Ищем кнопку закрытия попапа в блоке popup
+const popupOpenButtonElement = document.querySelector('.profile-info__edit-button'); // Ищем кнопку открытия попапа
+const profileNameElement = document.querySelector('.profile-info__name'); //Ищем строку с именем профиля
+const profileProfessionElement = document.querySelector('.profile-info__profession'); //Ищем строку с профессией профиля
+const formElement = popupElement.querySelector('.popup__form'); //Ищем форму в блоке popup
+const nameInput = formElement.querySelector('input[name="name"]'); //Ищем инпут для имени в форме
+const jobInput = formElement.querySelector('input[name="job"]'); //Ищем инпут для профессии в форме 
+
+const openPopup = function () {
+    popupElement.classList.add('popup_is-opened');
+    nameInput.value = profileNameElement.textContent;
+    jobInput.value = profileProfessionElement.textContent;
+} //Создаём функцию добавления класса для попапа, чтобы он открывался и чтобы инпуты в форме попапа 
+  //приняли текстовые значения из блока профиля для имени и професии
+
+const closePopup = function () {
+    popupElement.classList.remove('popup_is-opened');
+} //Создаём функцию удаления класса для попапа, чтобы он закрывался
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileNameElement.textContent = nameInput.value;
+    profileProfessionElement.textContent = jobInput.value;
+    closePopup();
+} //Функция, которая вносит изменения в имя и профессию в блоке профиля, записывая данные которые вписываются в инпуты в попапе
+
+popupOpenButtonElement.addEventListener('click', openPopup); //Слушатель, который запускает функцию открытия попапа по клику на кнопке edit
+popupCloseButtonElement.addEventListener('click', closePopup); //Слушатель, который запускает функцию закрытия попапа по клику close
+formElement.addEventListener('submit', formSubmitHandler); //Слушатель, который ждет когда в форме попапа (formElement) произойдет событие submit
+// затем запускает функцию, которая сохранит новые записи в инпутах формы в попапе и закроет окно попапа
+
+
+
+
